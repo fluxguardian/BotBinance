627f478f20b626ef360ebf5306c017b1af53b213
Flax <flaxmine9@gmail.com>
2021-02-25 22:52:18 +0300

    added a WaitCross

--- Strategy/Scalp.cs
+++ Strategy/Scalp.cs
@@ -51,6 +51,10 @@ namespace Strategy
             _asset = await _stock.GetExchangeInformationAsync(_symbol);
             _normalization = new Normalization(_asset);
 
+            List<Balance> balance = await _stock.GetBalance(_asset);
+
+            Console.WriteLine($"Баланс пользователя {_name}: {Math.Round(balance.First().Free, _normalization.Round.RoundPrice)} $");
+
             for (ushort i = 0; i < ushort.MaxValue; i++)
             {
                 try
@@ -66,12 +70,44 @@ namespace Strategy
                     else
                     {
                         Console.WriteLine("Ожидаем выход из рынка");
+
                         await Sell(profitPercent: 1.03m, stopLoss: 1.02m);
+
+                        await Task.Delay(2500);
+
+                        #region Баланс после продажи
+
+                        balance = await _stock.GetBalance(_asset);
+
+                        Console.WriteLine($"Баланс пользователя {_name}: {Math.Round(balance.First().Free, _normalization.Round.RoundPrice)} $");
+
+                        #endregion
+
+                        // ожидаем пересечение скользящих (сверху-вниз), чтобы след покупка была только после пересечении (снизу-верх)
+                        await WaitCross();
                     }
 
                     await Task.Delay((60 - DateTime.Now.Second + 2) * 1000);
                 }
                 catch (Exception e) { Console.WriteLine(e.Message); }
+            }
+        }
+
+        private async Task WaitCross()
+        {
+            Console.WriteLine("Ожидаем пересечение скользящих (сверху-вниз), чтобы след покупка была только после пересечении (снизу-верх)");
+
+            for (int i = 0; i < ushort.MaxValue; i++)
+            {
+                IEnumerable<Candlestick> candles = await _stock.GetCandlestickAsync(_symbol, _timeInterval, _sma.LongPeriod + 3);
+
+                bool signalSell = _sma.SellSignal(candles.Select(x => x.Close));
+                if (signalSell)
+                {
+                    break;
+                }
+
+                await Task.Delay((60 - DateTime.Now.Second + 2) * 1000);
             }
         }
 
@@ -92,7 +128,7 @@ namespace Strategy
                         Symbol = _symbol,
                         Side = OrderSide.SELL,
                         Quantity = _normalization.NormalizeSell(balance.Last().Free),
-                        Price = Math.Round(currentPrice + 1.0m, _normalization.Round.RoundPrice),
+                        Price = Math.Round(currentPrice + 1.2m, _normalization.Round.RoundPrice),
                         StopLoss = Math.Round(currentPrice - 0.6m, _normalization.Round.RoundPrice),
                         StopLimitPrice = Math.Round(currentPrice - 0.65m, _normalization.Round.RoundPrice)
                     }, _timeInterval);
@@ -127,7 +163,6 @@ namespace Strategy
                 await Task.Delay((60 - DateTime.Now.Second + 2) * 1000);
             }
         }
-
         private async Task Buy()
         {
             for (ushort i = 0; i < ushort.MaxValue; i++)
@@ -137,10 +172,10 @@ namespace Strategy
                     IEnumerable<Candlestick> candles = await _stock.GetCandlestickAsync(_symbol, _timeInterval, _sma.LongPeriod + 3);
                     IEnumerable<decimal> prices = candles.Select(x => x.Close);
 
-                    bool buySignalSma = _sma.BuySignalCross(prices);
-                    //bool buySignalTSI = _tsi.BuySignal(prices);
+                    bool buySignalSma = _sma.BuySignal(prices);
+                    bool buySignalTSI = _tsi.BuySignal(prices);
 
-                    if (buySignalSma) //&& buySignalTSI)
+                    if (buySignalSma && buySignalTSI)
                     {
                         List<Balance> balance = await _stock.GetBalance(_asset);
                         await _stock.TakeMarketOrder(new Signal()
