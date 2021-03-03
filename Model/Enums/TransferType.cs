31319da837eae580112a5858e55f21bfcc30cd91
Flax <flaxmine9@gmail.com>
2021-03-03 16:22:04 +0300

    added transfer types

---
+++ Model/Enums/TransferType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace Model.Enums
+{
+    public enum TransferType
+    {
+        [Description("1")]
+        ToMarginAccount,
+
+        [Description("2")]
+        ToMainAccount
+    }
+}
