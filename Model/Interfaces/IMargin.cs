65b2f552d6a98c5d838ff2095b144754346828ee
Flax <flaxmine9@gmail.com>
2021-03-03 16:22:36 +0300

    margin methods

---
+++ Model/Interfaces/IMargin.cs
@@ -0,0 +1,17 @@
+﻿using Model.Enums;
+using Model.Models.Account;
+using System.Threading.Tasks;
+
+namespace Model.Interfaces
+{
+    public interface IMargin
+    {
+        Task<AccountTransfer> Transfer(string asset, decimal amount, TransferType transferType);
+        Task<AccountBorrow> Borrow(string asset, decimal amount, string isIsolated = "FALSE");
+        Task<AccountRepay> Repay(string asset, decimal amount, string isIsolated = "FALSE");
+        Task<MaxBorrow> MaxBorrow(string asset);
+        Task<NewOrderMargin> MarketOrderQuantityMargin(string symbol, decimal quantity, OrderSide side, string isIsolated = "FALSE");
+        Task<NewOrderMargin> MarketOrderQuoteMargin(string symbol, decimal quantity, OrderSide side, string isIsolated = "FALSE");
+        Task<MaxTransferOutAmount> MaxTransferOutAmount(string asset, string isIsolated = "FALSE");
+    }
+}
