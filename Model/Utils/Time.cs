bfabbd430d7f4dfdf1082e25ad9091adba03d27c
Flax <flaxmine9@gmail.com>
2021-03-12 00:05:29 +0300

    get time sleep

---
+++ Model/Utils/Time.cs
@@ -0,0 +1,15 @@
+﻿using Model.Models.Market;
+using System;
+
+namespace Model.Utils
+{
+    public static class Time
+    {
+        public static int GetTimeSleepMilliseconds(this Candlestick candle)
+        {
+            var closeTime = DateTimeOffset.FromUnixTimeMilliseconds(candle.CloseTime).LocalDateTime;
+
+            return (int)(closeTime - DateTime.Now).TotalMilliseconds;
+        }
+    }
+}
