bb74fe141abc520c329ec1c4e82c78bb71ee588c
Flax <flaxmine9@gmail.com>
2021-03-14 01:24:46 +0300

    deleted static

--- TechnicalAnalysis/Oscillators/TrueStrengthIndex.cs
+++ TechnicalAnalysis/Oscillators/TrueStrengthIndex.cs
@@ -18,7 +18,7 @@ namespace TechnicalAnalysis.Oscillators
             _quantityCandles = quantityCandles;
         }
 
-        public static TSIValues GetTSI(List<decimal> prices, int quantityCandles, int periodTSI, int periodSignalLine)
+        public TSIValues GetTSI(List<decimal> prices)
         {
             try
             {
@@ -30,15 +30,14 @@ namespace TechnicalAnalysis.Oscillators
                 for (int i = 0; i < prices.Count - 1; i++)
                 {
                     momentum.Add(prices[i + 1] - prices[i]);
+                    absMomentum.Add(Math.Abs(prices[i + 1] - prices[i]));
                 }
 
-                absMomentum = momentum.Select(x => Math.Abs(x)).ToList();
+                var firstSmoothed = ExponentialMovingAverage.GetExponentialMovingAverages(momentum, _quantityCandles);
+                var secondSmoothed = ExponentialMovingAverage.GetExponentialMovingAverages(firstSmoothed, _periodTSI);
 
-                var firstSmoothed = ExponentialMovingAverage.GetExponentialMovingAverages(momentum, quantityCandles);
-                var secondSmoothed = ExponentialMovingAverage.GetExponentialMovingAverages(firstSmoothed, periodTSI);
-
-                var firstAbsSmoothed = ExponentialMovingAverage.GetExponentialMovingAverages(absMomentum, quantityCandles);
-                var secondAbsSmoothed = ExponentialMovingAverage.GetExponentialMovingAverages(firstAbsSmoothed, periodTSI);
+                var firstAbsSmoothed = ExponentialMovingAverage.GetExponentialMovingAverages(absMomentum, _quantityCandles);
+                var secondAbsSmoothed = ExponentialMovingAverage.GetExponentialMovingAverages(firstAbsSmoothed, _periodTSI);
 
                 for (int i = 0; i < secondSmoothed.Count; i++)
                 {
@@ -48,7 +47,7 @@ namespace TechnicalAnalysis.Oscillators
                 return new TSIValues()
                 {
                     TSI = tsi,
-                    SignalLine = ExponentialMovingAverage.GetExponentialMovingAverages(tsi, periodSignalLine)
+                    SignalLine = ExponentialMovingAverage.GetExponentialMovingAverages(tsi, _periodSignalLine)
                 };
             }
             catch { Console.WriteLine("Ошибка в методе GetTSI"); throw new Exception("Ошибка в методе GetTSI"); }
@@ -58,7 +57,7 @@ namespace TechnicalAnalysis.Oscillators
         {
             var pricesClose = prices.ToList();
 
-            TSIValues tsi = GetTSI(pricesClose, _quantityCandles, _periodTSI, _periodSignalLine);
+            TSIValues tsi = GetTSI(pricesClose);
             if(tsi.TSI.SkipLast(1).Last() > 0.15m && tsi.SignalLine.SkipLast(1).Last() > 0.1m)
             {
                 return true;
