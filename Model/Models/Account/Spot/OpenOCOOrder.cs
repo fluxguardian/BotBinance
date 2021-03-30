f6386ad058fdba8beb9da9236d783e5611d43dc3
Flax <flaxmine9@gmail.com>
2021-03-31 00:31:29 +0300

    open oco orderds

---
+++ Model/Models/Account/Spot/OpenOCOOrder.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Model.Models.Account.Spot
+{
+    public class OpenOCOOrder
+    {
+        [JsonProperty("orderListId")]
+        public long OrderListId { get; set; }
+        
+        [JsonProperty("contingencyType")]
+        public string ContingencyType { get; set; }
+
+        [JsonProperty("listStatusType")]
+        public string ListStatusType { get; set; }
+        
+        [JsonProperty("listOrderStatus")]
+        public string ListOrderStatus { get; set; }
+        
+        [JsonProperty("listClientOrderId")]
+        public string ListClientOrderId { get; set; }
+        
+        [JsonProperty("transactionTime")]
+        public long TransactionTime { get; set; }
+        
+        [JsonProperty("symbol")]
+        public string Symbol { get; set; }
+        
+        [JsonProperty("orders")]
+        public List<QueryOrder> Orders { get; set; }
+    }
+}
