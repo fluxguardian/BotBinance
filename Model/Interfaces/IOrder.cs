a5edef2e8de2693c17ccf747666d8edcc4465f82
Flax <flaxmine9@gmail.com>
2021-04-06 03:07:02 +0300

    added a new methods

--- Model/Interfaces/IOrder.cs
+++ Model/Interfaces/IOrder.cs
@@ -8,16 +8,18 @@ namespace Model.Interfaces
     public interface IOrder
     {
         Task<IEnumerable<Order>> GetAllOrders(string symbol, long? orderId = null, int limit = 500, long recvWindow = 5000);
-        Task<IEnumerable<Order>> GetCurrentOpenOrders(string symbol, long recvWindow = 5000);
-        Task<CanceledOrder> CancelOrder(string symbol, long? orderId = null, string origClientOrderId = null, long recvWindow = 5000);
-        Task<Order> GetOrder(string symbol, long? orderId = null, string origClientOrderId = null, long recvWindow = 5000);
+        Task<IEnumerable<Order>> GetCurrentOpenOrders(string symbol);
+        Task<CanceledOrder> CancelOrder(string symbol, long? orderId = null);
+        Task<Order> GetOrder(string symbol, long? orderId = null, string origClientOrderId = null);
         Task<NewOrder> PostNewOrder(string symbol, decimal quantity, decimal price, OrderSide side, OrderType orderType, TimeInForce timeInForce,
             decimal icebergQty = 0m, long recvWindow = 5000);
+        Task<NewOrder> OrderStopLossLimit(string symbol, decimal quantity, decimal stopLimitPrice, decimal stopPrice, OrderSide side);
+
         Task<OCOOrder> PostOCOOrder(string symbol, decimal quantity, decimal price, decimal stopPrice, decimal stopLimitPrice,
             OrderSide side, TimeInForce timeInForce = TimeInForce.GTC, long recvWindow = 5000);
         Task<QueryOCO> QueryOCO(long orderListId);
         Task<NewOrder> OrderQuantityMarket(string symbol, decimal quantity, OrderSide side);
-        Task<NewOrder> OrderQuoteMarket(string symbol, decimal quoteOrderQty, OrderSide side);
+        Task<NewOrder> OrderQuoteMarket(string symbol, decimal quoteOrderQty, OrderSide side);     
         Task<QueryOCO> CancelOCO(string symbol, string listClientOrderId);
         Task<List<OpenOCOOrder>> QueryOpenOCO();
     }
