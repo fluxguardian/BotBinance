037fc0b1455e0d1754d73063471adb430234478e
Flax <flaxmine9@gmail.com>
2021-04-06 03:07:24 +0300

    edited

--- Model/Models/Account/Spot/NewOrder.cs
+++ Model/Models/Account/Spot/NewOrder.cs
@@ -6,10 +6,13 @@ namespace Model.Models.Account.Spot
     {
         [JsonProperty("symbol")]
         public string Symbol { get; set; }
+
         [JsonProperty("orderId")]
         public long OrderId { get; set; }
+
         [JsonProperty("clientOrderId")]
         public string ClientOrderId { get; set; }
+
         [JsonProperty("transactTime")]
         public long TransactTime { get; set; }
     }
