0de159e8fb32f587a8acb37143c20595e1e89344
Flax <flaxmine9@gmail.com>
2021-04-18 01:18:45 +0300

    edited

---
+++ Csv/CSV.cs
@@ -0,0 +1,30 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Csv
+{
+    public static class CSV
+    {
+        public static List<DataStrategy> ReadCsv(string path)
+        {
+            CsvConfiguration configuration = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                Delimiter = ";",
+                
+            };
+
+            using (StreamReader streamReader = new StreamReader(path))
+            {
+                using (CsvReader csvReader = new CsvReader(streamReader, configuration))
+                {
+                    return csvReader.GetRecords<DataStrategy>().ToList();
+                }
+            }
+        }
+    }
+}
--- Strategy/LastStrategy.cs
+++ Strategy/LastStrategy.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using TechnicalAnalysis.Interfaces;
 using System.Linq;
 using System.Globalization;
+using Csv;
 
 namespace Strategy
 {
@@ -35,55 +36,25 @@ namespace Strategy
         public async Task Logic()
         {
             var res = ReadCsv(appPapth + @"\Data\DataLastStrategy\DataOfIndicators.csv");
+
+            Console.WriteLine(res.First().LrSlope);
+            Console.WriteLine(res.First().ProfitLong.ToString(CultureInfo.InvariantCulture));
         }
 
         private List<DataStrategy> ReadCsv(string path)
         {
-            
-            List<DataStrategy> result = File.ReadAllLines(path).Skip(1)
-                .Select(line => line.Split(';'))
-                .Select(x => new DataStrategy()
-                {
-                    LrSlope = Convert.ToDecimal(x[0]),
-                    ProfitLong = Convert.ToDecimal(x[1])
-                }).ToList();
+
+            //List<DataStrategy> result = File.ReadAllLines(path).Skip(1)
+            //    .Select(line => line.Split(';'))
+            //    .Select(x => new DataStrategy()
+            //    {
+            //        LrSlope = Convert.ToDecimal(x[0]),
+            //        ProfitLong = Convert.ToDecimal(x[1].Replace('.', ','))
+            //    }).ToList();
+
+            var result = CSV.ReadCsv(path);
 
             return result;
         }
-    }
-
-    public class DataStrategy
-    {
-        #region Periods
-
-        public decimal LrSlope { get; set; }
-        public decimal RSI { get; set; }
-        public decimal TSIFirstR { get; set; }
-        public decimal TSISecondS { get; set; }
-
-        #endregion
-
-        #region Values Long
-
-        public decimal RsiValueLong { get; set; }
-        public decimal SlopeValueLong { get; set; }   
-        public decimal TSIValueLong { get; set; }
-
-        #endregion
-
-        #region Values Short
-
-        public decimal RsiValueShort { get; set; }
-        public decimal SlopeValueShort { get; set; }
-        public decimal TSIValueShort { get; set; }
-
-        #endregion
-
-        #region Profit
-
-        public decimal ProfitLong { get; set; }
-        public decimal ProfitShort { get; set; }
-
-        #endregion
-    }
+    } 
 }
