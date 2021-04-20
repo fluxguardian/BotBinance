d47b57edb22a306be41cf038a1acb103285545fc
Flax <flaxmine9@gmail.com>
2021-04-20 20:21:48 +0300

    key and secret key

--- BotBinanceBL/Keys/Settings.cs
+++ BotBinanceBL/Keys/Settings.cs
@@ -2,10 +2,7 @@
 {
     public static class Settings
     {
-        public static string Key { get; set; } = "PRnI7XmHLHLe9JKHpyPsHiCeCWEXvLOZhMb2hEIBHFRWGqcJVdtoDu9nMnryKkRQ";
-        public static string SecretKey { get; set; } = "oCpgsvSbFL1jtAVa9wSv5VTNTWbp46mmcjSbZFw2Rv307qpnJymigQVILroH68MS";
-
-        public static string KirillKey { get; set; } = "4H61eVeOBlPGTYE0dsrPRPEN4ZcAY2jjNwzI6pWo1scJS2LkcXdRcscifBGRedC5";
-        public static string KirillSecretKey { get; set; } = "vsu9lyxO4zmKZNrNxIVNqnVntmTUJW4zMVYCHtPuaJkWSycgxCjTnAQy6V8t4TFa";
+        public static string Key { get; set; } = "";
+        public static string SecretKey { get; set; } = "";
     }
 }
